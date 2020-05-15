4819b39f1a8f781e51985c2224da57cd30d7a441
leonardofaria00 <leonardofaria00@gmail.com>
2020-05-15 18:08:43 -0300

    [UPDATE] - Atualizando Imagem

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,32 @@
 # VERSION v0.0.1
 # AUTHOR:         Leonardo Santos <leonardofaria00@gmail.com>
 # DESCRIPTION:    Image CentOS with PHP 7.3 and httpd 2.4
-# TO_BUILD:       docker-compose build
-# TO_RUN:         docker-compose up -d
+#
+# TO_BUILD:       docker build --pull --rm -f "Dockerfile" -t base-glpi:latest "."
+# TO_RUN:         docker run --rm -it  -p 80:80/tcp base-glpi:latest
 #
 # Dockerfile de construção do container WebApp utilizado pelo MD
 #
 # Container preparado e configurado para uso em desenvolvimento e testes
 ################################################################################
 
-#TODO
+# TODO
 FROM leonardofaria00/httpd-base_webapp:latest
+
+# Instalando Pacotes de Dependências especificas para o GLPI
+RUN yum -y --setopt=tsflags=nodocs update
+
+RUN yum -y --setopt=tsflags=nodocs install \
+    php-ldap \
+    php-xmlrpc \
+    php-imap \
+    # php-cas \
+    # php-curl \
+    # php-apcu-bc \
+    php-devel \
+    unixodbc-devel
+
+# RUN pecl install pdo_sqlsrv sqlsrv
 
 # Copia e executa o script de instalação do Projeto
 COPY app-start.sh /opt/
--- app-start.sh
+++ app-start.sh
@@ -1,38 +1,53 @@
 #!/bin/bash
 
-# Instalando dependências do PHP
-# yum install # GOTO
+# Controle du choix de version ou prise de la latest
+[[ ! "$VERSION_GLPI" ]] &&
+	VERSION_GLPI=$(curl -s https://api.github.com/repos/glpi-project/glpi/releases/latest | grep tag_name | cut -d '"' -f 4)
 
-# Removendo arquivos do document root
-rm -rf /var/www/html/*
-
-# Controle de versão, verifica se é a latest
-[[ ! "$VERSION_APP" ]] \
-	&& VERSION_APP=$(curl -s https://api.github.com/repos/glpi-project/glpi/releases/latest | grep tag_name | cut -d '"' -f 4)
-
-SRC_APP=$(curl -s https://api.github.com/repos/glpi-project/glpi/releases/tags/${VERSION_APP} | jq .assets[0].browser_download_url | tr -d \")
-TAR_APP=$(basename ${SRC_APP})
-FOLDER_APP=app/
+SRC_GLPI=$(curl -s https://api.github.com/repos/glpi-project/glpi/releases/tags/${VERSION_GLPI} | jq .assets[0].browser_download_url | tr -d \")
+TAR_GLPI=$(basename ${SRC_GLPI})
+FOLDER_GLPI=glpi/
 FOLDER_WEB=/var/www/html/
 
-# Download e extração de fontes da Aplicação
-if [ "$(ls ${FOLDER_WEB}${FOLDER_APP})" ];
-then
-	echo "Aplication is already installed"
-else
-	wget -P ${FOLDER_WEB} ${SRC_APP}
-	tar -xzf ${FOLDER_WEB}${TAR_APP} -C ${FOLDER_WEB}
-	rm -Rf ${FOLDER_WEB}${TAR_APP}
-	chown -R www-data:www-data ${FOLDER_WEB}${FOLDER_APP}
-	rm -rf /var/www/html/github.com
-	mv /var/www/html/glpi/* /var/www/html
+# Check if TLS_REQCERT is present
+if !(grep -q "TLS_REQCERT" /etc/ldap/ldap.conf); then
+	echo "TLS_REQCERT isn't present"
+	echo -e "TLS_REQCERT\tnever" >>/etc/ldap/ldap.conf
 fi
 
-# Incluindo tarefas agendadas pelo cron e ativando
-echo "*/2 * * * * www-data /usr/bin/php /var/www/html/front/cron.php &>/dev/null" >> /etc/cron.d/app
+# Téléchargement et extraction des sources de GLPI
+if [ "$(ls ${FOLDER_WEB}${FOLDER_GLPI})" ]; then
+	echo "Aplication is already installed"
+else
+	# Removendo arquivos do document root
+	rm -rf /var/www/html/*
 
-# Start cron service
+	wget -P ${FOLDER_WEB} ${SRC_GLPI}
+	tar -xzf ${FOLDER_WEB}${TAR_GLPI} -C ${FOLDER_WEB}
+	rm -Rf ${FOLDER_WEB}${TAR_GLPI}
+	chown -R www-data:www-data ${FOLDER_WEB}${FOLDER_GLPI}
+	mv /var/www/html/glpi/* /var/www/html
+	chmod 777 -R /var/www/html/
+
+fi
+
+# Modificando vhost para default
+echo -e "<VirtualHost *:80>\n\tDocumentRoot /var/www/html/glpi\n\n\t<Directory /var/www/html/glpi>\n\t\tAllowOverride All\n\t\tOrder Allow,Deny\n\t\tAllow from all\n\t</Directory>\n\n\tErrorLog /var/log/apache2/error-glpi.log\n\tLogLevel warn\n\tCustomLog /var/log/apache2/access-glpi.log combined\n</VirtualHost>" >/etc/apache2/sites-available/000-default.conf
+
+#Add scheduled task by cron and enable
+echo "*/2 * * * * www-data /usr/bin/php /var/www/html/glpi/front/cron.php &>/dev/null" >>/etc/cron.d/glpi
+
+#Start cron service
 service cron start
 
-# Serviço Apache em primeiro plano
+# Habilitar extension sqlsrv e pdo_sqlsrv
+echo "extension=/usr/lib/php/20180731/sqlsrv.so" >>/etc/php/7.3/apache2/php.ini
+echo "extension=/usr/lib/php/20180731/pdo_sqlsrv.so" >>/etc/php/7.3/apache2/php.ini
+echo "extension=/usr/lib/php/20180731/sqlsrv.so" >>/etc/php/7.3/cli/php.ini
+echo "extension=/usr/lib/php/20180731/pdo_sqlsrv.so" >>/etc/php/7.3/cli/php.ini
+
+# Ativando rewrite no apache2
+a2enmod rewrite && service apache2 restart && service apache2 stop
+
+#Lancement du service apache au premier plan
 exec /usr/sbin/httpd -D FOREGROUND
