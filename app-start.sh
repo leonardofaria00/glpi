255bf85bf9a509c35320345696b594d170104ad6
leonardofaria00 <leonardofaria00@gmail.com>
2020-08-04 19:02:52 -0300

    [UPDATE] - Removendo seção

--- app-start.sh
+++ app-start.sh
@@ -6,5 +6,8 @@ FOLDER_WEB=/var/www/html/
 # Definindo dono do document root
 chown -R apache:apache ${FOLDER_WEB}
 
+# Removendo seção
+rm -rf app/files/_sessions/*
+
 # Executando aplicação
 exec /usr/sbin/httpd -D FOREGROUND
